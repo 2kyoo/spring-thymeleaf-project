665c269dff3c5789a59cd2a135298fd2b29f1a4f
2kyoo <bladeoftown@yahoo.com>
2024-09-06 16:59:04 +0300

    first commit

---
+++ src/test/java/org/p2kyoo/springthymeleafproject/SpringThymeleafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.p2kyoo.springthymeleafproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
